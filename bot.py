fcc047b0e434845cf60646a43e75232ad2cd5399
a2kat <a2kat_90@mail.ru>
2023-07-16 10:43:45 +0300

    Added a handler for text messages

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ class BetBot(telebot.TeleBot):
 		super().__init__(token=config.TELEGRAM_TOKEN, parse_mode=None)
 		self.set_my_commands(commands=BetBot.MENU_COMMANDS)
 
-		self.scheduler = Scheduler()
+	# self.scheduler = Scheduler()
 
 	def send_admin_message(self, text):
 		self.send_message(chat_id=config.ADMIN_ID, text=text)
@@ -35,7 +35,7 @@ bot = BetBot()
 
 
 @bot.message_handler(func=lambda msg: msg.text[1:] in [c.command for c in bot.get_my_commands()])
-def handle_start_help(message):
+def handle_commands(message):
 	if message.text == '/start':
 		response_message = config.BOT_START_MESSAGE
 	elif message.text == '/help':
@@ -43,6 +43,14 @@ def handle_start_help(message):
 	else:
 		response_message = 'THIS COMMAND IS NOT FUNCTIONAL YET'
 	bot.send_admin_message(response_message)
+
+
+@bot.message_handler(func=lambda msg: True)
+def handle_messages(message):
+	bot.send_admin_message('Текстовые сообщения ботом не принимаются.\n'
+						   'Пожалуйста, воспользуйтесь командами из поддерживаемого списка.\n'
+						   '\n'
+						   'Для вывода списка нажмите /help')
 
 
 if __name__ == '__main__':
