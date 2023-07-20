487d7abb62752e0b7758aeb2a68d055546c02a4e
a2kat <a2kat_90@mail.ru>
2023-07-20 23:31:46 +0300

    Made Scheduler class a part of BetBot class Added a job method Added _schedule_test_messaging method Added typehints

--- scheduler.py
+++ scheduler.py
@@ -1,41 +1,37 @@
 from time import sleep
 import schedule
 from datetime import datetime, time
+from typing import Tuple
 
 
 class Scheduler:
 
-	def __init__(self):
-		pass
+	def __init__(self, bot):
+		self.bot = bot
+		self._schedule_test_messaging()
 
-	def _print_message_once(self, text):
-		print(f'{datetime.now().strftime("%H:%M:%S")}: {text}')
-		return schedule.CancelJob
+	def _job(self, text: str) -> None:
+		msg1 = f'{datetime.now().strftime("%H:%M:%S")}\n'
+		msg2 = 'Scheduled message:\n'
+		msg3 = f'{text}'
+		self.bot.send_admin_message(text=''.join([msg1, msg2, msg3]))
 
-	def schedule_message(self, text: str, hour: int, minute: int, second: int):
+	# return schedule.CancelJob
+
+	def _schedule_message_daily(self, text: str, hour: int, minute: int, second: int) -> None:
 		time_obj = time(hour, minute, second)
-		schedule.every().day.at(time_obj.strftime("%H:%M:%S")).do(self._print_message_once, text=text)
+		schedule.every().day.at(time_obj.strftime("%H:%M:%S")).do(self._job, text=text)
+
+	def _schedule_test_messaging(self) -> None:
+		t1: Tuple[int, int, int] = (8, 0, 00)
+		t2: Tuple[int, int, int] = (12, 0, 00)
+		t3: Tuple[int, int, int] = (17, 0, 00)
+
+		self._schedule_message_daily(text='Доброе утро! Бот на связи', hour=t1[0], minute=t1[1], second=t1[2])
+		self._schedule_message_daily(text='Обед! Bon appetit!', hour=t2[0], minute=t2[1], second=t2[2])
+		self._schedule_message_daily(text='Рабочий день закончен. Хорошего вечера!', hour=t3[0], minute=t3[1], second=t3[2])
 
 	def start(self):
 		while True:
 			schedule.run_pending()
 			sleep(1)
-
-
-s = Scheduler()
-
-dt1 = (21, 54, 7)
-dt2 = (21, 54, 22)
-dt3 = (21, 54, 50)
-
-for dt in (dt1, dt2, dt3):
-	hour, minute, second = dt
-	s.schedule_message(
-		text="working ...",
-		hour=hour,
-		minute=minute,
-		second=second
-	)
-print(schedule.jobs)
-
-s.start()
