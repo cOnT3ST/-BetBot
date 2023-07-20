4ed4904b2b4cbaf486662669957b92d1833e6222
a2kat <a2kat_90@mail.ru>
2023-07-20 22:47:39 +0300

    Added typehints

--- bot.py
+++ bot.py
@@ -2,25 +2,28 @@ import os
 import telebot
 from dotenv import load_dotenv
 import config
+from typing import List, Tuple
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
 
+
 # TODO all methods in this file must have type hints -> check for it
 
 class BetBot(telebot.TeleBot):
 	# Commands available in bot menu:
-	MENU_COMMANDS_TEXT = [
+	MENU_COMMANDS_TEXT: List[Tuple[str, str]] = [
 		('start', 'Запустить бота'),
 		('help', 'Перечень доступных команд'),
 		('admin', 'Функционал администратора')
 	]
 
-	MENU_COMMANDS = [telebot.types.BotCommand(comm, desc) for comm, desc in MENU_COMMANDS_TEXT]
+	MENU_COMMANDS: List[telebot.types.BotCommand] = \
+		[telebot.types.BotCommand(comm, desc) for comm, desc in MENU_COMMANDS_TEXT]
 
 	# Admin only commands available after inputting 'admin' command:
-	ADMIN_COMMANDS_TEXT = [
+	ADMIN_COMMANDS_TEXT: List[str] = [
 		'Some func №1'
 		'Some func №2'
 		'Some func №3'
@@ -30,9 +33,7 @@ class BetBot(telebot.TeleBot):
 		super().__init__(token=TELEGRAM_TOKEN, parse_mode=None)
 		self.set_my_commands(commands=BetBot.MENU_COMMANDS)
 
-	# self.scheduler = Scheduler()
-
-	def send_admin_message(self, text):
+	def send_admin_message(self, text: str) -> None:
 		self.send_message(chat_id=ADMIN_ID, text=text)
 
 
@@ -40,7 +41,7 @@ bot = BetBot()
 
 
 @bot.message_handler(func=lambda msg: msg.text[1:] in [c.command for c in bot.get_my_commands()])
-def handle_commands(message):
+def handle_commands(message) -> None:
 	if message.text == '/start':
 		response_message = config.BOT_START_MESSAGE
 	elif message.text == '/help':
@@ -51,11 +52,13 @@ def handle_commands(message):
 
 
 @bot.message_handler(func=lambda msg: True)
-def handle_messages(message):
-	bot.send_admin_message('Текстовые сообщения ботом не принимаются.\n'
-						   'Пожалуйста, воспользуйтесь командами из поддерживаемого списка.\n'
-						   '\n'
-						   'Для вывода списка нажмите /help')
+def handle_messages(message) -> None:
+	bot.send_admin_message(
+		'Текстовые сообщения ботом не принимаются.\n'
+		'Пожалуйста, воспользуйтесь командами из поддерживаемого списка.\n'
+		'\n'
+		'Для вывода списка нажмите /help'
+	)
 
 
 if __name__ == '__main__':
