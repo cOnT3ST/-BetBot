2bf1bb38b0d242a7d9716937cbfb58eb8029d81e
a2kat <a2kat_90@mail.ru>
2023-07-21 21:54:48 +0300

    Added a timezone because deploy app obviously has another so scheduling is 3 hours delayed.

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,8 @@ from time import sleep
 import schedule
 from datetime import datetime, time
 from typing import Tuple
+from pytz import timezone
+from config import SCHEDULER_TIMEZONE
 
 
 class Scheduler:
@@ -9,6 +11,7 @@ class Scheduler:
 	def __init__(self, bot):
 		self.bot = bot
 		self._schedule_test_messaging()
+		self.timezone = timezone(SCHEDULER_TIMEZONE)
 
 	def _job(self, text: str) -> None:
 		msg1 = f'{datetime.now().strftime("%H:%M:%S")}\n'
@@ -20,12 +23,16 @@ class Scheduler:
 
 	def _schedule_message_daily(self, text: str, hour: int, minute: int, second: int) -> None:
 		time_obj = time(hour, minute, second)
-		schedule.every().day.at(time_obj.strftime("%H:%M:%S")).do(self._job, text=text)
+		schedule.every().day.at(time_obj.strftime("%H:%M:%S"), tz=self.timezone).\
+			do(self._job, text=text)
 
 	def _schedule_test_messaging(self) -> None:
-		t1: Tuple[int, int, int] = (8, 0, 00)
-		t2: Tuple[int, int, int] = (12, 0, 00)
-		t3: Tuple[int, int, int] = (17, 0, 00)
+		# t1: Tuple[int, int, int] = (8, 0, 00)
+		# t2: Tuple[int, int, int] = (12, 0, 00)
+		# t3: Tuple[int, int, int] = (17, 0, 00)
+		t1: Tuple[int, int, int] = (22, 1, 00)
+		t2: Tuple[int, int, int] = (22, 2, 00)
+		t3: Tuple[int, int, int] = (22, 3, 00)
 
 		self._schedule_message_daily(text='Доброе утро! Бот на связи', hour=t1[0], minute=t1[1], second=t1[2])
 		self._schedule_message_daily(text='Обед! Bon appetit!', hour=t2[0], minute=t2[1], second=t2[2])
