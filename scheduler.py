0a4fe19aec719aa9a43f739ce0a2b778e1cb4544
a2kat <a2kat_90@mail.ru>
2023-07-28 01:05:46 +0300

    Shedule message daily now takes str time

--- scheduler.py
+++ scheduler.py
@@ -9,6 +9,7 @@ import os
 load_dotenv()
 SCHEDULER_TIMEZONE: str = os.getenv("SCHEDULER_TIMEZONE")
 
+
 class Scheduler:
 
 	def __init__(self, bot):
@@ -16,25 +17,33 @@ class Scheduler:
 		self.timezone = timezone(SCHEDULER_TIMEZONE)
 		self._schedule_test_messaging()
 
-
 	def _job(self, text: str) -> None:
-		msg1 = f'{datetime.now().strftime("%H:%M:%S")}\n'
-		msg2 = 'Scheduled message:\n'
-		msg3 = f'{text}'
-		self.bot.send_admin_message(text=''.join([msg1, msg2, msg3]))
+		msg = f'{datetime.now().strftime("%H:%M:%S")}\n' \
+			f'Scheduled message:\n' \
+			f'{text}'
+		self.bot.send_admin_message(text=msg)
 
 	# return schedule.CancelJob
 
-	def _schedule_message_daily(self, text: str, hour: int, minute: int, second: int) -> None:
-		time_obj = time(hour, minute, second)
-		schedule.every().day.at(time_obj.strftime("%H:%M:%S"), tz=self.timezone).\
+	def _schedule_message_daily(self, text: str, run_time: str) -> None:
+		time_obj = time.fromisoformat(run_time)
+		schedule.every().day.at(time_obj.strftime("%H:%M:%S"), tz=self.timezone). \
 			do(self._job, text=text)
 
 	def _schedule_test_messaging(self) -> None:
 		t1, t2, t3 = SCHEDULER_RUN_TIMES
-		self._schedule_message_daily(text='Доброе утро! Бот на связи', hour=t1[0], minute=t1[1], second=t1[2])
-		self._schedule_message_daily(text='Обед! Bon appetit!', hour=t2[0], minute=t2[1], second=t2[2])
-		self._schedule_message_daily(text='Рабочий день закончен. Хорошего вечера!', hour=t3[0], minute=t3[1], second=t3[2])
+		self._schedule_message_daily(
+			text='Доброе утро! Бот на связи',
+			run_time=t1
+		)
+		self._schedule_message_daily(
+			text='Обед! Bon appetit!',
+			run_time=t2
+		)
+		self._schedule_message_daily(
+			text='Рабочий день закончен. Хорошего вечера!',
+			run_time=t3
+		)
 
 	def start(self):
 		while True:
