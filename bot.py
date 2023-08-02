6ffe7b31c0e934a4377acc41a423c7d138ea036d
cOnT3ST <45936979+cOnT3ST@users.noreply.github.com>
2023-08-02 14:34:49 +0300

    Some stuff

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import telebot
 from dotenv import load_dotenv
-import config
+from config import BOT_START_MESSAGE, BOT_HELP_MESSAGE
 from typing import List, Tuple, Optional, Callable
 from scheduler import Scheduler
 import threading
@@ -9,9 +9,7 @@ import threading
 load_dotenv()
 TELEGRAM_TOKEN: str = os.getenv("TEST_TELEGRAM_TOKEN")
 #TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-# TODO get rid of test account id and read it from db
-TEST_ACCOUNT_ID = int(os.getenv("TEST_ACCOUNT_ID"))
+ADMIN_ID: str = os.getenv("ADMIN_ID")
 
 
 class BetBot(telebot.TeleBot):
@@ -27,41 +25,31 @@ class BetBot(telebot.TeleBot):
 
     # Admin only commands available after inputting 'admin' command:
     ADMIN_COMMANDS_TEXT: List[str] = [
-        'Показать доступные турниры',
+        'Команда 1',
         'Команда 2',
         'Команда 3',
-        'Команда 4',
-        'Команда 5',
-        'Команда 6',
-        'Команда 7',
-        'Команда 8',
-        'Команда 9',
-        'Команда 10',
     ]
 
     def __init__(self):
         super().__init__(token=TELEGRAM_TOKEN, parse_mode=None)
-        self.scheduler = Scheduler(bot=self)
+        #self.scheduler = Scheduler(bot=self)
 
         self.set_my_commands(commands=BetBot.MENU_COMMANDS)
         self.commands = self.get_available_commands()
-        self.allowed_users: List[int] = [ADMIN_ID, TEST_ACCOUNT_ID]
-        self.register_message_handler(self.handle_commands, commands=self.commands)
-        self.register_message_handler(self.handle_messages)
-        self.register_callback_query_handler(
-            callback=self.handle_admin_callbacks,
-            func=lambda query: 'admin' in query.data
-        )
+        self.allowed_users: List[int] = [int(ADMIN_ID)]
+        #self.register_message_handler(self.handle_command, commands=self.commands)
+        self.register_message_handler(self.handle_message)
 
-        self.scheduler_thread = threading.Thread(target=self.scheduler.start)
+        #self.scheduler_thread = threading.Thread(target=self.scheduler.start)
         self.thread = threading.Thread(target=self.start)
-        self.scheduler_thread.start()
+        #self.scheduler_thread.start()
         self.thread.start()
 
     def get_available_commands(self) -> List[str]:
-        return [c.command for c in self.get_my_commands()]
+        return [f'/{c.command}' for c in self.get_my_commands()]
 
     def send_admin_message(self, text: str) -> None:
+        #telebot.TeleBot.send_message(chat_id=ADMIN_ID)
         self.send_message(chat_id=ADMIN_ID, text=text)
 
     def start(self):
@@ -81,6 +69,7 @@ class BetBot(telebot.TeleBot):
         """
         def wrapper(self, message):
             if message.from_user.id not in self.allowed_users:
+                self.delete_message(message.chat.id, message.id)
                 self.send_message(message.chat.id, "Доступ запрещен.\n\nВы не являетесь участником соревнования.")
             else:
                 message_handler(self, message)
@@ -88,61 +77,31 @@ class BetBot(telebot.TeleBot):
         return wrapper
 
     @authorized_users
-    def handle_commands(self, message: telebot.types.Message) -> None:
-        if message.text == '/start':
-            response_message = config.BOT_START_MESSAGE
-        elif message.text == '/help':
-            response_message = 'Это бот используется для автоматизации нашего соревнования ' \
-                               'по ставкам.\n\nКоманды в чат можно писать самому при помощи слэша' \
-                               ' (например /help), либо выбирать из меню, либо кликать по уже' \
-                               ' указанным в чате.\n\nДоступные команды:\n'
-            for c in self.get_my_commands():
-                response_message += f'/{c.command} - {c.description}\n'
-            response_message += '\nПо всем вопросам - пишите или звоните\nСпасибо за использование!'
-        elif message.text == '/admin':
-            if message.from_user.id != ADMIN_ID:
-                response_message = 'Этот функционал доступен только администратору бота'
-            else:
-                ik = self.create_admin_inline()
-                self.send_message(message.from_user.id, 'Выберите требуемую команду:', reply_markup=ik)
-                return
-        else:
-            response_message = 'Эта команда пока не поддерживается'
-        self.send_message(message.from_user.id, response_message)
-
-    @authorized_users
-    def handle_messages(self, message: telebot.types.Message) -> None:
+    def handle_message(self, message: telebot.types.Message) -> None:
         if message.content_type not in ['text']:
             response_message = 'Этот бот принимает не принимает сообщения такого типа.'
         elif message.text.startswith('/'):
-            response_message = 'Похоже, вы ввели неподдерживаемую команду.'
+            response_message = self.handle_command(message)
         else:
             response_message = 'Текстовые сообщения ботом не принимаются'
-        response_ending = "\n\n" \
-                          "Пожалуйста, воспользуйтесь командами из поддерживаемого списка.\n" \
-                          "Для вывода списка нажмите /help"
-        self.send_message(message.from_user.id, response_message + response_ending)
+        self.send_message(message.from_user.id, response_message)
 
-    def create_admin_inline(self) -> telebot.types.InlineKeyboardMarkup:
-        inline_keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
-        buttons = []
-        for c in BetBot.ADMIN_COMMANDS_TEXT:
-            button = telebot.types.InlineKeyboardButton(text=c, callback_data=f'admin_button{len(buttons) + 1}')
-            buttons.append(button)
-        inline_keyboard.add(*buttons)
-        return inline_keyboard
-
-    def handle_admin_callbacks(self, callback_query: telebot.types.CallbackQuery) -> None:
-        chat_id = callback_query.from_user.id
-        message_id = callback_query.message.id
-
-        if callback_query.data == 'admin_button1':
-            self.send_message(chat_id, f'Здесь будет функционал кнопки {callback_query.data}')
+    def handle_command(self, message: telebot.types.Message) -> str:
+        if message.text not in self.commands:
+            response_message = 'Похоже, вы ввели неподдерживаемую команду.' \
+                               '\n\n' \
+                               'Пожалуйста, воспользуйтесь командами из поддерживаемого списка.' \
+                               '\n' \
+                               'Для вывода списка нажмите /help"'
+        elif message.text == '/start':
+            response_message = BOT_START_MESSAGE
+        elif message.text == '/help':
+            commands_n_descriptions = ''.join([f'/{c.command} - {c.description}\n' for c in self.get_my_commands()])
+            response_message = BOT_HELP_MESSAGE.format(commands_n_descriptions)
         else:
-            self.send_message(chat_id, 'Эта команда пока не поддерживается')
+            response_message = 'Эта команда пока не поддерживается'
+        return response_message
+    
 
-        self.edit_message_text(chat_id=chat_id, message_id=message_id, text=f'You pressed {callback_query.data}')
-
-
-
-bot = BetBot()
+if __name__ == "__main__":
+    bot = BetBot()
---
+++ stats_api.py
@@ -0,0 +1,72 @@
+import datetime
+
+from dotenv import load_dotenv
+import os
+from urllib.parse import urljoin
+import requests
+from typing import Union
+from config import DEFAULT_COUNTRY, DEFAULT_LEAGUE
+from pytz import timezone
+from pprint import pprint
+
+load_dotenv()
+SCHEDULER_TIMEZONE: str = os.getenv("SCHEDULER_TIMEZONE")
+STAT_API_KEY = os.getenv("STAT_API_KEY")
+STAT_API_BASE_URL = 'https://api-football-beta.p.rapidapi.com'
+STAT_API_HOST = 'api-football-beta.p.rapidapi.com'
+HEADERS = {"X-RapidAPI-Key": STAT_API_KEY, "X-RapidAPI-Host": STAT_API_HOST}
+
+
+class StatsAPI:
+    def __init__(self):
+        self.timezone = timezone(SCHEDULER_TIMEZONE)
+
+    def get_contest_info(self,
+                         country: str = DEFAULT_COUNTRY,
+                         league: str = DEFAULT_LEAGUE,
+                         year: int = None
+                         ) -> Union[dict, None]:
+        if not year:
+            year = datetime.datetime.now(tz=self.timezone).year
+        endpoint = 'leagues'
+        querystring = {"country": country, "name": league, "season": year}
+        response = requests.get(urljoin(STAT_API_BASE_URL, endpoint), headers=HEADERS, params=querystring)
+        valued_data = response.json()['response'][0]
+        return valued_data if not valued_data == [] else None
+
+    def get_matches_info(
+            self,
+            league_api_id: int,
+            year: int,
+            round_id=None
+    ):
+        endpoint = 'fixtures'
+        next_year = year + 1
+        querystring = {
+            "from": f"{year}-07-01",
+            "to": f"{next_year}-07-01",
+            "timezone": self.timezone,
+            "season": year,
+            "league": league_api_id,
+            "round": round_id if not round_id else f"Regular Season - {str(round_id)}"
+        }
+        response = requests.get(
+            urljoin(STAT_API_BASE_URL, endpoint),
+            headers=HEADERS,
+            params=querystring
+        )
+        valued_data = response.json()['response']
+        return valued_data
+
+    def get_all_contest_matches(self, league_api_id: int, year: int) -> list:
+        return self.get_matches_info(league_api_id, year)
+
+    def get_contest_matches_by_round(self, league_api_id: int, year: int, round_id: int) -> list:
+        return self.get_matches_info(league_api_id, year, round_id)
+
+
+if __name__ == '__main__':
+    sah = StatsAPI()
+    league_api_id = sah.get_contest_info()['league']['id']
+    year = sah.get_contest_info()['seasons'][0]['year']
+    pprint(sah.get_matches_info(league_api_id=league_api_id, year=year))
