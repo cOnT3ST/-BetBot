b6bf58208f2175d356a9d5619ad0b810a2ca1cbe
cOnT3ST <45936979+cOnT3ST@users.noreply.github.com>
2023-11-20 20:37:16 +0300

    Create contest feature added

--- bot.py
+++ bot.py
@@ -5,18 +5,21 @@ from dotenv import load_dotenv
 from config import BOT_START_MESSAGE, BOT_HELP_MESSAGE, BOT_ACCESS_DENIED_MESSAGE, BOT_UNSUPPORTED_COMMAND_MESSAGE, \
     BOT_ADMIN_COMMANDS_DENIED_MESSAGE, BOT_UNSUPPORTED_MESSAGE_TYPE_MESSAGE, BOT_COMMAND_NOT_SUPPORTED_MESSAGE, \
     DEFAULT_COUNTRY, DEFAULT_LEAGUE
-from typing import List, Tuple, Optional, Callable, Union
+from typing import List, Tuple, Optional, Callable, Union, Dict
 from scheduler import Scheduler
 from stats_api import StatsAPI
 from database import Database
 import threading
-from pprint import pprint
 
+
+
+# TODO use load_confidential_data.py
 load_dotenv()
 TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
 # TODO get rid of test account id and read it from db
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 TEST_ACCOUNT_ID = int(os.getenv("TEST_ACCOUNT_ID"))
+
 
 class BetBot(telebot.TeleBot):
     # Commands available in bot menu:
@@ -26,12 +29,13 @@ class BetBot(telebot.TeleBot):
         ('admin', 'Функционал администратора')
     ]
 
-    MENU_COMMANDS: List[telebot.types.BotCommand] = \
+    # Transforms MENU_COMMANDS_TEXT into a list of telebot command type
+    MENU_TELEBOT_COMMANDS: List[telebot.types.BotCommand] = \
         [telebot.types.BotCommand(comm, desc) for comm, desc in MENU_COMMANDS_TEXT]
 
     # Admin only commands available after inputting 'admin' command:
     ADMIN_COMMANDS_TEXT: List[str] = [
-        'Команда 1',
+        'Создать соревнование',
         'Команда 2',
         'Команда 3'
     ]
@@ -42,9 +46,10 @@ class BetBot(telebot.TeleBot):
         self.api = StatsAPI()
         self.db = Database()
 
-        self.set_my_commands(commands=BetBot.MENU_COMMANDS)
+        self.set_my_commands(commands=BetBot.MENU_TELEBOT_COMMANDS)
         self.commands = self.get_available_commands()
-        self.allowed_users: List[int] = [ADMIN_ID]
+        self.allowed_users_ids: List[int] = [ADMIN_ID]
+
         self.register_message_handler(self.handle_message)
         self.register_callback_query_handler(
             callback=self.handle_admin_callback,
@@ -57,9 +62,11 @@ class BetBot(telebot.TeleBot):
         self.bot_thread.start()
 
     def get_available_commands(self) -> List[str]:
+        """Returns a list of / commands available for all users"""
         return [f'/{c.command}' for c in self.get_my_commands()]
 
     def send_admin_message(self, text: str) -> None:
+        # Sends message to admin only
         self.send_message(chat_id=ADMIN_ID, text=text)
 
     def start(self):
@@ -77,12 +84,14 @@ class BetBot(telebot.TeleBot):
             Callable: A decorated handler.
 
         """
+
         def wrapper(self, message):
-            if message.from_user.id not in self.allowed_users:
+            if message.from_user.id not in self.allowed_users_ids:
                 self.delete_message(message.chat.id, message.id)
                 self.send_message(message.chat.id, BOT_ACCESS_DENIED_MESSAGE)
             else:
                 message_handler(self, message)
+
         return wrapper
 
     @authorized_users
@@ -96,8 +105,11 @@ class BetBot(telebot.TeleBot):
             response_message = 'Текстовые сообщения ботом не принимаются'
         self.send_message(message.from_user.id, response_message, reply_markup=keyboard)
 
-    def handle_command(self, message: telebot.types.Message) \
-            -> tuple[str, Union[telebot.types.InlineKeyboardMarkup, None]]:
+    def handle_command(
+            self,
+            message: telebot.types.Message
+    ) -> tuple[str, Union[telebot.types.InlineKeyboardMarkup, None]]:
+
         keyboard = None
         if message.text not in self.commands:
             response_message = BOT_UNSUPPORTED_COMMAND_MESSAGE
@@ -122,10 +134,10 @@ class BetBot(telebot.TeleBot):
 
         if callback_query.data == 'admin_button1':
             self.create_contest()
+        elif callback_query.data == 'admin_button2':
+            self.send_message(chat_id, BOT_COMMAND_NOT_SUPPORTED_MESSAGE)
         else:
             self.send_message(chat_id, BOT_COMMAND_NOT_SUPPORTED_MESSAGE)
-
-        self.edit_message_text(chat_id=chat_id, message_id=message_id, text=f'You pressed {callback_query.data}')
 
     def create_admin_inline(self) -> telebot.types.InlineKeyboardMarkup:
         inline_keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
@@ -137,26 +149,52 @@ class BetBot(telebot.TeleBot):
         return inline_keyboard
 
     def create_contest(self) -> None:
-        current_year = datetime.datetime.now(tz=self.scheduler.timezone).year
-        if self.db.contest_exists(DEFAULT_LEAGUE, DEFAULT_COUNTRY, current_year):
-            self.send_admin_message(f'Соревнование РПЛ {current_year} года уже есть в базе данных')
+        """Creates a betting contest for the current season"""
+
+        if not self.api.country_supported(DEFAULT_COUNTRY):
+            self.send_admin_message(
+                f'Не удалось найти {DEFAULT_COUNTRY} в базе данных сервиса статистики.\n'
+                f'Возможно, поддержка страны прекращена'
+            )
             return
 
-        data = self.api.get_contest_info()
-        api_id = data['league']['id']
-        name = data['league']['name']
-        country = data['country']['name']
-        year = data['seasons'][0]['year']
-        start_datetime = data['seasons'][0]['start']
-        finish_datetime = data['seasons'][0]['end']
-        logo = data['league']['logo']
-        current = data['seasons'][0]['current']
+        current_season = self.api.determine_current_season()
+        print("1")
+        if not current_season:
+            self.send_admin_message(f'Текущий сезон в сервисе статистики пока не стартовал.')
+            return
 
-        contest_info = [api_id, name, country, year, start_datetime, finish_datetime, logo, current]
+        self.db.create_contests_table()
+        if self.db.contest_exists(current_season['contest_api_id'], current_season['year']):
+            self.send_admin_message(
+                f"Такое соревнование сезона {current_season['year']}-{current_season['year'] + 1}"
+                f" уже есть в базе данных"
+            )
+            return
+        print("2")
+        self.db.add_contest(**current_season)
+        print("3")
+        self.db.create_bets_table()
+        print("4")
+        self.create_calendar(current_season)
+        self.send_admin_message(
+            f"Соревнование по ставкам {current_season['league_country']} {current_season['league_name']}"
+            f" сезона {current_season['year']}-{current_season['year'] + 1} создано!"
+        )
 
-        self.db.add_contest(*contest_info)
-        self.send_admin_message(f'Соревнование {country} {name} {year} создано!')
+    def create_calendar(self, season: Dict[str, Union[str, int]]):
+        teams_list = self.api.get_all_teams(season['contest_api_id'], season['year'])
+        self.send_admin_message("Список команд сезона успешно загружен с сайта статистики")
+        self.db.create_teams_table()
+        self.db.update_teams_list(teams_list)
+        self.send_admin_message("Список команд сезона обновлен в базе данных")
+        full_calendar = self.api.get_calendar(season)
+        self.send_admin_message("Календарь успешно загружен с сайта статистики")
+        self.db.create_matches_table()
+        self.db.update_matches(full_calendar)
+        self.send_admin_message("Календарь успешно добавлен в базу данных")
 
 
 if __name__ == "__main__":
     bot = BetBot()
+
