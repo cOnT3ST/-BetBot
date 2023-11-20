8353e14d0b6498613340bb600d7f90b356911ef6
cOnT3ST <45936979+cOnT3ST@users.noreply.github.com>
2023-11-20 20:37:04 +0300

    Create contest feature added

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-import pprint
 import sqlite3 as sq
 from typing import List, Dict, Union, Callable, Any
 import datetime
@@ -7,125 +6,189 @@ from pprint import pprint
 
 
 class Database:
-	def __init__(self):
-		self.name = 'db/MyRPLBetBot.db'
+    def __init__(self):
+        self.name = 'db/MyRPLBetBot.db'
 
-	@staticmethod
-	def _ensure_connection(func: Callable):
-		def wrapper(self, *args: Any, **kwargs: Any) -> Callable:
-			with sq.connect(self.name) as conn:
-				cur = conn.cursor()
-				res = func(self, *args, **kwargs, conn=conn, cur=cur)
-			return res
-		return wrapper
+    @staticmethod
+    def _ensure_connection(func: Callable):
+        def wrapper(self, *args: Any, **kwargs: Any) -> Callable:
+            with sq.connect(self.name) as conn:
+                cur = conn.cursor()
+                res = func(self, *args, **kwargs, conn=conn, cur=cur)
+            return res
 
-	@_ensure_connection
-	def add_contest(self, contest_api_id: int, name: str, country: str, year: int, start_datetime: str,
-					finish_datetime: str, logo: str, is_active, conn: sq.Connection, cur: sq.Cursor) -> None:
-		if self.contest_exists(name, country, year):
-			return
+        return wrapper
 
-		query = '''
+    @_ensure_connection
+    def _read_table_column_names(self, table_name: str, conn: sq.Connection, cur: sq.Cursor) -> list[str]:
+        cur.execute(f"PRAGMA table_info({table_name})")
+        rows = cur.fetchall()
+        column_names = [row[1] for row in rows]
+        return column_names
+
+    @_ensure_connection
+    def create_teams_table(self, conn: sq.Connection, cur: sq.Cursor):
+        query = '''
+		CREATE TABLE IF NOT EXISTS "teams" (
+			"team_id"	INTEGER NOT NULL UNIQUE,
+			"name"	TEXT,
+			"city"	TEXT,
+			"logo_url"	TEXT,
+			"logo"	BLOB,
+			PRIMARY KEY("team_id")
+			)
+		'''
+        cur.execute(query)
+        conn.commit()
+
+    @_ensure_connection
+    def _delete_table(self, table_name: str, conn: sq.Connection, cur: sq.Cursor):
+        query = f"DROP TABLE IF EXISTS {table_name}"
+        cur.execute(query)
+        conn.commit()
+
+    @_ensure_connection
+    def _add_team(self, team: dict, conn: sq.Connection, cur: sq.Cursor):
+
+        query = '''
+		INSERT OR IGNORE INTO teams (team_id, name, city, logo_url, logo)
+		VALUES(?, ?, ?, ?, ?)
+		'''
+        params = (team['team_id'], team['name'], team['city'], team['logo_url'], team['logo'])
+
+        cur.execute(query, params)
+        conn.commit()
+
+    def update_teams_list(self, teams_to_check: List[Dict]):
+        for t in teams_to_check:
+            self._add_team(t)
+
+    @_ensure_connection
+    def create_contests_table(self, conn: sq.Connection, cur: sq.Cursor):
+        query = '''
+		CREATE TABLE IF NOT EXISTS "contests" (
+		"contest_id"	INTEGER NOT NULL,
+		"contest_api_id"	INTEGER,
+		"league_name"	TEXT,
+		"league_country"	TEXT,
+		"year"	INTEGER,
+		"start_date"	TEXT,
+		"finish_date"	TEXT,
+		"logo"	BLOB,
+		"logo_url"	TEXT,
+		"is_active"	INTEGER NOT NULL DEFAULT 1,
+		PRIMARY KEY("contest_id" AUTOINCREMENT)
+		)
+		'''
+        cur.execute(query)
+        conn.commit()
+
+    @_ensure_connection
+    def add_contest(self, contest_api_id: int, league_name: str, league_country: str, year: int, start_date: str,
+                    finish_date: str, logo: bytes, logo_url: str, is_active: bool, conn: sq.Connection,
+					cur: sq.Cursor)\
+			-> None:
+        query = '''
 		INSERT INTO contests
-		(api_id, name, logo, country, year, start_datetime, finish_datetime, logo, is_active)
+		(contest_api_id, league_name, league_country, year, start_date, finish_date, logo, logo_url, is_active)
 		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
 		'''
-		params = (contest_api_id, name, logo, country, year, start_datetime, finish_datetime, logo, is_active)
-		cur.execute(query, params)
-		conn.commit()
+        params = (contest_api_id, league_name, league_country, year, start_date, finish_date, logo, logo_url, is_active)
+        cur.execute(query, params)
+        conn.commit()
 
-	@_ensure_connection
-	def contest_exists(self, name: str, country: str, year: int, conn: sq.Connection, cur: sq.Cursor) -> bool:
-		"""
-		Checks if content already exists in the database.
-		"""
-		query = "SELECT * FROM contests WHERE name = ? AND country = ? AND year = ?"
-		params = (name, country, year)
-		cur.execute(query, params)
-		result = cur.fetchall()
-		return result != []
+    @_ensure_connection
+    def contest_exists(self, contest_api_id: int, year: int, conn: sq.Connection, cur: sq.Cursor) -> bool:
 
-	@_ensure_connection
-	def delete_contest(self, name: str, country: str, year: int, conn: sq.Connection, cur: sq.Cursor) -> None:
-		if not self.contest_exists(name, country, year):
-			return
-		query = '''DELETE FROM contests WHERE name = ? AND country = ? AND year = ?'''
-		params = (name, country, year)
-		cur.execute(query, params)
-		conn.commit()
-		print(f'Сontest {name} {country} season {year} deleted')
+        query = "SELECT * FROM contests WHERE contest_api_id = ? AND year = ?"
+        params = (contest_api_id, year)
+        cur.execute(query, params)
+        result = cur.fetchall()
+        return result != []
 
-	@_ensure_connection
-	def read_contest(self, id: int, conn: sq.Connection, cur: sq.Cursor) -> Union[tuple, None]:
-		query = '''SELECT * FROM contests WHERE id = ?'''
-		params = (id,)
-		cur.execute(query, params)
-		res = cur.fetchone()
-		return res
+    @_ensure_connection
+    def read_contest(self, contest_id, conn: sq.Connection, cur: sq.Cursor):
+        query = '''SELECT * FROM contests WHERE contest_id = ?'''
+        params = (contest_id,)
+        cur.execute(query, params)
+        row_data = cur.fetchall()
+        contest = dict(zip([c[0] for c in cur.description], row_data[0]))
+        return contest
 
-	# @_ensure_connection
-	# def get_users(self, conn, cur) -> List:
-	# 	query = "SELECT * FROM users"
-	# 	cur.execute(query)
-	# 	db_data = cur.fetchall()
-	# 	return [
-	# 		{
-	# 			'id': user[0],
-	# 			'first_name': user[1],
-	# 			'last_name': user[2],
-	# 			'telegram_id': user[3],
-	# 			'is_admin': True if user[4] == 1 else False,
-	# 			'contest': None
-	# 		}
-	# 		for user in db_data
-	# 	]
-	#
-	# @_ensure_connection
-	# def get_contests(self, conn, cur):
-	# 	query = "SELECT * FROM contest"
-	# 	cur.execute(query)
-	# 	db_data = cur.fetchall()
-	# 	return [
-	# 		{
-	# 			'id': contest[0],
-	# 			'api_id': contest[1],
-	# 			'name': contest[2],
-	# 			'country': contest[3],
-	# 			'year': contest[4],
-	# 			'start_datetime': contest[5],
-	# 			'finish_datetime': contest[6],
-	# 			'logo': contest[7],
-	# 			'is_active': True if contest[8] == 1 else False
-	# 		}
-	# 		for contest in db_data
-	# 	]
-	# #[(2, 235, None, None, 2023, None, None, None, 0)]
+    @_ensure_connection
+    def delete_contest(self, name: str, country: str, year: int, conn: sq.Connection, cur: sq.Cursor) -> None:
+        if not self.contest_exists(name, country, year):
+            return
+        query = '''DELETE FROM contests WHERE name = ? AND country = ? AND year = ?'''
+        params = (name, country, year)
+        cur.execute(query, params)
+        conn.commit()
+        print(f'Сontest {name} {country} season {year} deleted')
 
-	# def add_calendar(self, data: List[Dict], contest_id=1):
-	# 	# res = []
-	# 	with sq.connect(self.name) as conn:
-	# 		cur = conn.cursor()
-	# 		for match in data:
-	# 			match_id = match['fixture']['id']
-	# 			match_datetime = datetime.datetime.fromisoformat(match['fixture']['date']),
-	# 			home_team_id = match['teams']['home']['id'],
-	# 			away_team_id = match['teams']['away']['id'],
-	# 			venue_id = match['fixture']['venue']['id'],
-	# 			score = f"{match['goals']['home']}-{match['goals']['away']}",
-	# 			status_short = match['fixture']['status']['short'],
-	# 			status_long = match['fixture']['status']['long'],
-	# 			league_round = match['league']['round']
-	#
-	# 			cur.execute("INSERT INTO matches "
-	# 						"(match_id, contest_id, datetime, home_team_id, away_team_id, venue_id, score, status_short,"
-	# 						"round, status_long)"
-	# 						"VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-	# 						(match_id, contest_id, match_datetime, home_team_id, away_team_id, venue_id, score,
-	# 						 status_short, league_round, status_long))
-	# 			conn.commit()
+    @_ensure_connection
+    def create_matches_table(self, conn: sq.Connection, cur: sq.Cursor):
+        query = '''
+		CREATE TABLE IF NOT EXISTS "matches" (
+		"match_id"	INTEGER NOT NULL UNIQUE,
+		"contest_api_id"	INTEGER,
+		"match_datetime"	TEXT,
+		"round"	INTEGER,
+		"home_team_id"	INTEGER,
+		"away_team_id"	INTEGER,
+		"score"	TEXT,
+		"status_long"	TEXT,
+		"status_short"	TEXT,
+		FOREIGN KEY("contest_api_id") REFERENCES "contests"("contest_api_id"),
+		PRIMARY KEY("match_id")
+		)
+		'''
+        cur.execute(query)
+        conn.commit()
+
+    @_ensure_connection
+    def _update_match(self, match: Dict[str, Union[str, int]], conn: sq.Connection, cur: sq.Cursor) -> None:
+        query = '''
+		INSERT INTO matches
+		(match_id, contest_api_id, match_datetime, round, home_team_id, away_team_id, score, status_long, status_short)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+		'''
+        params = (
+            match['match_id'], match['contest_api_id'], match['match_datetime'], match['round'], match['home_team_id'],
+            match['away_team_id'], match['score'], match['status_long'], match['status_short']
+        )
+        cur.execute(query, params)
+        conn.commit()
+
+    def update_matches(self, matches_list: List[Dict]) -> None:
+        for m in matches_list:
+            self._update_match(m)
+
+    @_ensure_connection
+    def create_bets_table(self, conn: sq.Connection, cur: sq.Cursor) -> None:
+        query = '''
+        CREATE TABLE IF NOT EXISTS "bets" (
+        "match_id"	INTEGER,
+        "user_id"	INTEGER,
+        "bet"	TEXT,
+        FOREIGN KEY("user_id") REFERENCES "users"("user_id"),
+        FOREIGN KEY("match_id") REFERENCES "calendar"("match_id")
+        )
+        '''
+        cur.execute(query)
+        conn.commit()
+
+    @_ensure_connection
+    def _delete_all_tables(self, conn: sq.Connection, cur: sq.Cursor) -> None:
+        for t in ('contests', 'teams', 'matches', 'bets'):
+            self._delete_table(t)
+
 
 
 if __name__ == '__main__':
-	db = Database()
-	# db.delete_contest('Premier League', 'Russia', 2023)
+    from pprint import pprint
+
+    db = Database()
+    # for t in ('contests', 'bets', 'matches', 'teams'):
+    #     db._delete_table(t)
+    db._delete_all_tables()
 
--- stats_api.py
+++ stats_api.py
@@ -1,70 +1,214 @@
 import datetime
-
-from dotenv import load_dotenv
-import os
+import time
 from urllib.parse import urljoin
 import requests
-from typing import Union
+from typing import Union, List, Dict
 from config import DEFAULT_COUNTRY, DEFAULT_LEAGUE
+from load_confidential_data import load_confidentials_from_env
 from pytz import timezone
-from pprint import pprint
 
-load_dotenv()
-SCHEDULER_TIMEZONE: str = os.getenv("SCHEDULER_TIMEZONE")
-STAT_API_KEY = os.getenv("STAT_API_KEY")
+CONFIDENTIAL_DATA = load_confidentials_from_env()
 STAT_API_BASE_URL = 'https://api-football-beta.p.rapidapi.com'
 STAT_API_HOST = 'api-football-beta.p.rapidapi.com'
-HEADERS = {"X-RapidAPI-Key": STAT_API_KEY, "X-RapidAPI-Host": STAT_API_HOST}
+HEADERS = {"X-RapidAPI-Host": STAT_API_HOST, "X-RapidAPI-Key": CONFIDENTIAL_DATA['stat_api_key']}
 
 
 class StatsAPI:
     def __init__(self):
-        self.timezone = timezone(SCHEDULER_TIMEZONE)
+        self.timezone = timezone(CONFIDENTIAL_DATA['scheduler_timezone'])
+        # TODO add a counter for requests per day, erase it every day, keep in mind permitted
+        # requests per day by stats service
 
-    def get_contest_info(self,
-                         country: str = DEFAULT_COUNTRY,
-                         league: str = DEFAULT_LEAGUE,
-                         year: int = None
-                         ) -> Union[dict, None]:
-        if not year:
-            year = datetime.datetime.now(tz=self.timezone).year
-        endpoint = 'leagues'
-        querystring = {"country": country, "name": league, "season": year}
-        response = requests.get(urljoin(STAT_API_BASE_URL, endpoint), headers=HEADERS, params=querystring)
-        valued_data = response.json()['response'][0]
-        return valued_data if not valued_data == [] else None
+    def _make_request(self, endpoint: str, params: dict[str, Union[str, int]] = None) \
+            -> Union[None, dict]:
+        """
+        Makes request to a certain endpoint of the API stats server
 
-    # def get_matches_info(
-    #         self,
-    #         league_api_id: int,
-    #         year: int,
-    #         round_id=None
-    # ):
-    #     endpoint = 'fixtures'
-    #     next_year = year + 1
-    #     querystring = {
-    #         "from": f"{year}-07-01",
-    #         "to": f"{next_year}-07-01",
-    #         "timezone": self.timezone,
-    #         "season": year,
-    #         "league": league_api_id,
-    #         "round": round_id if not round_id else f"Regular Season - {str(round_id)}"
-    #     }
-    #     response = requests.get(
-    #         urljoin(STAT_API_BASE_URL, endpoint),
-    #         headers=HEADERS,
-    #         params=querystring
-    #     )
-    #     valued_data = response.json()['response']
-    #     return valued_data
-    #
-    # def get_all_contest_matches(self, league_api_id: int, year: int) -> list:
-    #     return self.get_matches_info(league_api_id, year)
-    #
-    # def get_contest_matches_by_round(self, league_api_id: int, year: int, round_id: int) -> list:
-    #     return self.get_matches_info(league_api_id, year, round_id)
+        :param endpoint: API endpoint
+        :param params: A set of parameters required for this particular request
+        :return:
+        """
+
+        if not isinstance(endpoint, str):
+            raise ValueError('Endpoint must be a string')
+
+        request_url = urljoin(base=STAT_API_BASE_URL, url=endpoint)
+        # request_url = "https://www.amazon.com/nothing_here"
+        # request_url = 'https://dadsasdasdsd.com/'
+        # request_url = 'https://api.github.com/'
+        # try:
+        # response = requests.get(request_url, headers=HEADERS, params=params)
+        # try:
+        #     response = requests.get('https://geeksforgeeks.org/naveen/')
+        #     response.raise_for_status()
+        # except requests.ConnectionError as e:
+        #     print(e.args[0])
+        # except requests.HTTPError as e:
+        #     print(e.args[0])
+        # except requests.TooManyRedirects as e:
+        #     print(e.args[0])
+        # except requests.ReadTimeout as e:
+        #     print(e.args[0])
+        # except requests.Timeout as e:
+        #     print(e.args[0])
+        # except requests.JSONDecodeError as e:
+        #     print(e.args[0])
+
+        response = requests.get(request_url, headers=HEADERS, params=params)
+
+        if not response.ok:
+            print('Request failed')
+            return None
+
+        valued_data = response.json()
+        print('Response ok. Data obtained')
+        return valued_data
+
+    def country_supported(self, country_name: str) \
+            -> bool:
+        """
+        Checks if a certain country is supported by the stats API
+        :return: True if country is supported, False otherwise
+        """
+
+        # TODO add supported countries to DB or cache to avoid excessive API requests and update it once per week
+        if not isinstance(country_name, str):
+            raise ValueError('Country parameter must only be string')
+
+        response = self._make_request(
+            endpoint='countries',
+            params={'name': country_name}
+        )
+
+        result = response['results'] != 0
+        return result
+
+    def determine_current_season(self) -> Union[None, Dict[str, Union[str, int]]]:
+        """
+        Determines the details of the current season for a football league.
+
+        Returns a dictionary containing the details of the current season, or None if the league hasn't started yet.
+
+        Returns:
+            Union[None, Dict[str, Union[str, int]]]: A dictionary with the following keys:
+                - 'contest_api_id' (int): The API ID of the current season.
+                - 'league_name' (str): The name of the league.
+                - 'league_country' (str): The country of the league.
+                - 'year' (int): The year of the current season.
+                - 'start_date' (str): The start date of the current season.
+                - 'end_date' (str): The end date of the current season.
+                - 'logo_url' (str): The URL of the league's logo.
+                - 'logo' (bytes): The downloaded logo image data.
+            If the league hasn't started yet, None is returned.
+
+        """
+
+        league_country = DEFAULT_COUNTRY
+        league_name = DEFAULT_LEAGUE
+
+        response = self._make_request(
+            endpoint='leagues',
+            params={
+                'name': league_name,
+                'current': 'true',
+                'country': league_country
+            }
+        )
+
+        if response['results'] == 0:  # League hasn't started yet
+            return None
+
+        season_id = response['response'][0]['league']['id']
+        logo_url = response['response'][0]['league']['logo']
+        logo = self._download_logo(logo_url)
+        season_data = response['response'][0]['seasons'][0]
+
+        return {
+            'contest_api_id': season_id,
+            'league_name': league_name,
+            'league_country': league_country,
+            'year': season_data['year'],
+            'start_date': season_data['start'],
+            'finish_date': season_data['end'],
+            'logo_url': logo_url,
+            'logo': logo,
+            'is_active': True
+        }
+
+    def get_calendar(self, contest: Dict[str, Union[str, int]]) -> List[Dict]:
+        """Get the match calendar for a contest.
+
+        Args:
+            contest (dict): Contest details including API ID, season, etc.
+
+        Returns:
+            list: List of dictionaries containing match details.
+
+        """
+
+        response = self._make_request(
+            endpoint='fixtures',
+            params={
+                #"from": contest['start_date'],
+                "from": "2023-07-21",
+                "to": contest['finish_date'],
+                "timezone": self.timezone.zone,
+                "season": contest['year'],
+                "league": contest['contest_api_id']
+            }
+        )
+
+        raw_matches_data = response['response']
+        matches = [
+            {
+                'match_id': m['fixture']['id'],
+                'contest_api_id': contest['contest_api_id'],
+                'match_datetime': m['fixture']['date'],
+                'round': int(m['league']['round'].split(' - ')[-1]),
+                'home_team_id': m['teams']['home']['id'],
+                'away_team_id': m['teams']['away']['id'],
+                'score': f"{m['goals']['home']}-{m['goals']['away']}",
+                'status_long': m['fixture']['status']['long'],
+                'status_short': m['fixture']['status']['short']
+            }
+            for m in raw_matches_data
+        ]
+        return matches
+
+    def get_all_teams(self, contest_api_id: int, year: int) \
+            -> List[Dict[str, Union[str, bytes]]]:
+        """Gets a list of teams to participate in this football tournament."""
+
+        response = self._make_request(
+            endpoint='teams',
+            params={
+                "league": contest_api_id,
+                "season": year
+            }
+        )
+
+        teams_list = response['response']
+        result = []
+        for t in teams_list:
+            team_id = t['team']['id']
+            name = t['team']['name']
+            city = t['venue']['city']
+            logo_url = t['team']['logo']
+            logo = self._download_logo(logo_url)
+            result.append({'team_id': team_id, 'name': name, 'city': city, 'logo': logo, 'logo_url': logo_url})
+
+        return result
+
+    def _download_logo(self, url: str) -> bytes:
+        response = requests.get(url)
+        if not response.ok:
+            # Get default logo
+            with open('db/Images/no logo.png', 'rb') as file:
+                return file.read()
+        return response.content
 
 
 if __name__ == '__main__':
-    sah = StatsAPI()
-    pprint(sah.get_contest_info())
+    from pprint import pprint
+
+    sa = StatsAPI()
